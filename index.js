5b212b3c43f0cab925e96e761e8ebd5104bc0683
Shubhkant Dwivedi <78783841+iamshubh22@users.noreply.github.com>
2021-11-29 13:13:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const {MongoClient} = require("mongodb");
-const client =new MongoClient("mongodb+srv://demon22:iamdemon22@cluster0.rhm8l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+const client =new MongoClient("mongodb+srv://demon22:<Password>@cluster0.rhm8l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
 client.connect().then((mClient)=>{
     const db=mClient.db();
     db.collection("book")
